5679139e92db56ac6c157ddb1c8b41a6cfa143a7
simon <simon@ALEXCOMPUTER>
2025-09-02 20:17:27 -0400

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+//Find Average of Values
+#include <iostream>
+using namespace std;
+
+int main()
+{
+//Variables
+	int num1, num2, num3, num4, num5;
+	num1 = 28; 
+	num2 = 32; 
+	num3 = 37; 
+	num4 = 24; 
+	num5 = 33;
+
+	//Calculate Sum
+	int sum = num1 + num2 + num3 + num4 + num5;
+
+	//Calculate Average
+	int average = sum / 5;
+
+	//Display Results
+	cout << "The sum is: " << sum << endl;
+	cout << "The average is: " << average << endl;
+
+	return 0;
+
+	}
+	
